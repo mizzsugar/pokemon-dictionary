da0b6f75ef0aaa9a751deddd57fcb9c32cedf54e
Mizuki Sato <mizki.sato.0025@gmail.com>
2022-05-04 10:02:26 +0900

    モンスターを全部返すAPIを作成

--- nuxt-app/server/api/monsters/index.ts
+++ nuxt-app/server/api/monsters/index.ts
@@ -1,5 +1,13 @@
-export default defineEventHandler((event) => {
-  return {
-    api: 'works'
-  }
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+
+export default defineEventHandler(async (event) => {
+  return await prisma.monster.findMany({
+    include: { types: { include: { type: true } } },
+  })
 })
