d17b8cf88a336aa30f54a496be3c67c822435d82
Mizuki Sato <mizki.sato.0025@gmail.com>
2022-05-05 11:41:15 +0900

    テーブル名、カラム名にpokemonを使うよう変更

---
+++ nuxt-app/repository/migrations/20220505022121_rename_to_pokemon/migration.sql
@@ -0,0 +1,3 @@
+-- This is an empty migration.
+ALTER TABLE "monster_types" RENAME TO "pokemon_types";
+ALTER TABLE "monsters" RENAME TO "pokemons";
---
+++ nuxt-app/repository/migrations/20220505023813_rename_to_pokemon_id/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "pokemon_types" RENAME COLUMN "monster_id" TO "pokemon_id";
